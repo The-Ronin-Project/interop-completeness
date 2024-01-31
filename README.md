eaeabc3d6a5c9cf6a4e56f995708291328f77496
William Sun <william@projectronin.com>
2024-01-30 16:08:25 -0800

    Added basic repository and gradle files

---
+++ README.md
@@ -0,0 +1,10 @@
+# interop-completeness
+
+### Additional Documentation Links
+
+- [Confluence - Design](https://projectronin.atlassian.net/wiki/spaces/ENG/pages/2162753606/Interops+Data+Completeness+Service+Design)
+- [Jira - Epic](https://projectronin.atlassian.net/browse/INT-2437)
+- [Jira - GraphQL API definition](https://projectronin.atlassian.net/browse/INT-2460)
+- [Jira - Directed Acyclic Graph (DAG) DB definition](https://projectronin.atlassian.net/browse/INT-2461)
+- [Jira - Runs & Loads DB definition](https://projectronin.atlassian.net/browse/INT-2469)
+- [Github - Event Contracts](https://github.com/projectronin/contract-event-interop-completeness)
---
+++ settings.gradle.kts
@@ -0,0 +1,33 @@
+rootProject.name = "interop-completeness"
+
+include("interop-completeness-server")
+include("interop-completeness-client")
+
+for (project in rootProject.children) {
+    project.buildFileName = "${project.name}.gradle.kts"
+}
+
+pluginManagement {
+    repositories {
+        maven {
+            url = uri("https://repo.devops.projectronin.io/repository/maven-snapshots/")
+            mavenContent {
+                snapshotsOnly()
+            }
+        }
+        maven {
+            url = uri("https://repo.devops.projectronin.io/repository/maven-releases/")
+            mavenContent {
+                releasesOnly()
+            }
+        }
+        maven {
+            url = uri("https://repo.devops.projectronin.io/repository/maven-public/")
+            mavenContent {
+                releasesOnly()
+            }
+        }
+        mavenLocal()
+        gradlePluginPortal()
+    }
+}
