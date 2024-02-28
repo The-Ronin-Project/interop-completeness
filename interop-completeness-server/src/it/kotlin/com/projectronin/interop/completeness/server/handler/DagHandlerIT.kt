0d2dba85cda976581f79f6142069e26c4e5f8cef
William Sun <william@projectronin.com>
2024-02-28 13:56:11 -0800

    INT-2548 added auth token to GraphQL request

--- interop-completeness-server/src/it/kotlin/com/projectronin/interop/completeness/server/handler/DagHandlerIT.kt
+++ interop-completeness-server/src/it/kotlin/com/projectronin/interop/completeness/server/handler/DagHandlerIT.kt
@@ -1,14 +1,16 @@
 package com.projectronin.interop.completeness.server.handler
 
-import com.expediagroup.graphql.client.spring.GraphQLWebClient
+import com.expediagroup.graphql.client.ktor.GraphQLKtorClient
 import com.projectronin.interop.completeness.client.generated.DAGQuery
 import com.projectronin.interop.completeness.server.BaseCompletenessIT
 import com.projectronin.interop.completeness.server.data.relational.DagDAO
+import io.ktor.client.request.bearerAuth
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.ktorm.database.Database
+import java.net.URL
 import java.time.OffsetDateTime
 
 class DagHandlerIT : BaseCompletenessIT() {
@@ -21,10 +23,13 @@ class DagHandlerIT : BaseCompletenessIT() {
         val eventTime = OffsetDateTime.now()
         dagDAO.replace("MedicationAdministration", listOf("Patient", "Medication"), eventTime)
 
-        val client = GraphQLWebClient(url = graphqlEndpoint)
+        val client = GraphQLKtorClient(URL(graphqlEndpoint))
+        val token = "test"
         val response =
             runBlocking {
-                client.execute(DAGQuery())
+                client.execute(DAGQuery()) {
+                    bearerAuth(token)
+                }
             }
         response.errors?.let { println("Found errors querying DAG: ${response.errors}") }
         response.data?.let { println("Found DAG: ${response.data}") }
